0cc516044e978c17376960f45132178fe7287e23
Juliet <agutu@gmail.com>
2022-04-14 16:41:11 +0300

    add url paths for accounts and register and add register settings on view

---
+++ neighbourhoodwatch/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('accounts/register/', views.register, name='register'),
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighbourhoodwatch/views.py
+++ neighbourhoodwatch/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+# from .models import Profile
 
 # Create your views here.
+
+def index(request):
+
+
+    return render(request, 'neighbourhoodwatch/index.html')
+
+
+
+def register(request):
+    form = UserCreationForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User has been registered')
+
+            return redirect ('/accounts/login')
+        else:
+            return render(request, 'registration/registration_form.html', {'form': form})
+
+    else:
+        return render(request, 'registration/registration_form.html', {'form': form})
