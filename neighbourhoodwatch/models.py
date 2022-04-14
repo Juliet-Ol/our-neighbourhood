ae6ac9b8e0bc921feea5c295f898fa28f44d74a3
Juliet <agutu@gmail.com>
2022-04-14 17:52:08 +0300

    create profile models and migrate

--- neighbourhoodwatch/models.py
+++ neighbourhoodwatch/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+    
+
+
+
+class Profile(models.Model):
+    name =models.CharField(max_length=50, blank=True)  
+    id = models.CharField(max_length=50, blank=True, primary_key=True)
+    # neighbourhood = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE, primary_key=True)
+    email = models.EmailField(null=True) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(max_length=300, default='') 
+
+    def __str__(self):
+        return f'{self.user.username} Profile'    
+
+         
+    
+     
+
+
+    
+    
