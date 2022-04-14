cebd7362b8817c109bf399ad0e6c39a637d473f9
Juliet <agutu@gmail.com>
2022-04-14 22:54:17 +0300

    create forms.py file and add profile form class, and views for profile and register in admin.py

--- neighbourhoodwatch/urls.py
+++ neighbourhoodwatch/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
+from neighbourhoodwatch import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/register/', views.register, name='register'),
+    path('profile', views.profile, name='profile'),
+    path('edit-profile', views.editProfile, name='edit-profile'),
     
 ]
 if settings.DEBUG:
--- neighbourhoodwatch/views.py
+++ neighbourhoodwatch/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-# from .models import Profile
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+
+from .forms import ProfileForm
 
 # Create your views here.
 
 def index(request):
+    profile_form=ProfileForm
 
 
-    return render(request, 'neighbourhoodwatch/index.html')
+    return render(request, 'neighbourhoodwatch/index.html', {"profile_form":profile_form})
 
 
 
@@ -27,3 +31,48 @@ def register(request):
 
     else:
         return render(request, 'registration/registration_form.html', {'form': form})
+        
+@login_required(login_url='login')
+def editProfile(request):
+    profile=Profile.objects.get(user= request.user.id)
+
+    form = ProfileForm(initial={'name':profile.name, 'bio':profile.bio, 'email':profile.email})
+
+   
+    return render(request, 'profile/edit.html', {'form': form})  
+
+
+def profile(request):
+    form = ProfileForm
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+             
+        profile=Profile.objects.get(user= request.user.id)
+       
+        if form.is_valid():
+
+          
+            profile.image=form.cleaned_data['image'] if len(request.FILES) != 0 else profile.image
+            profile.name=form.cleaned_data['name']  
+            # profile.id= form.cleaned_data['id']            
+            profile.email=form.cleaned_data['email'] 
+            profile.bio=form.cleaned_data['bio']             
+            profile.save()
+
+           
+            messages.success(request, 'Profile has been updated')
+
+            return redirect ('/profile')
+        else:
+            return render(request, 'profile/edit.html', {'form': form})
+
+    else:
+        
+
+        if Profile.objects.filter(user = request.user.id).count() == 0:
+            profile = Profile(user=request.user, name=request.user.username, email=request.user.email, bio='')
+            profile.save()
+        else:
+            profile= request.user.profile 
+        return render(request, 'profile/profile.html', {'form': form, 'profile':profile})    
