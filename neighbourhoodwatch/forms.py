11b35988217b701b30ddc47a79ac80d88550bb91
Juliet <agutu@gmail.com>
2022-04-15 19:29:12 +0300

    create models for post, add to forms register in admin.py and migrate our model

--- neighbourhoodwatch/forms.py
+++ neighbourhoodwatch/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Profile
+from .models import Post, Profile
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude =['user', 'id']
         fields = '__all__'
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['author', 'published_date']
+        fields = '__all__'       
--- neighbourhoodwatch/models.py
+++ neighbourhoodwatch/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 
@@ -17,7 +18,33 @@ class Profile(models.Model):
     bio = models.TextField(max_length=300, default='') 
 
     def __str__(self):
-        return f'{self.user.username} Profile'    
+        return f'{self.user.username} Profile'  
+
+class Post (models.Model):
+    title = models.CharField(max_length=20)
+    picture = CloudinaryField('image')
+    post = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    # neighbourhood = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE, primary_key=True)
+    
+
+    class Meta:
+        ordering = ['-published_date']  
+
+    @classmethod
+    def display(cls):
+        posts = cls.objects.all()
+        return posts
+
+    def save_posts(self):
+        self.user
+    def delete_posts(self):
+        self.delete()
+    @classmethod
+    def search_projects(cls, name):
+        return cls.objects.filter(title__icontains=name).all() 
+
 
          
     
