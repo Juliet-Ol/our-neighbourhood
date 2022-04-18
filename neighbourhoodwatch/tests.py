a31fc7e1fd1151f3a52643685a12a2ade2ab02b2
Juliet <agutu@gmail.com>
2022-04-18 19:22:15 +0300

    create tests

--- neighbourhoodwatch/tests.py
+++ neighbourhoodwatch/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from requests import post
+from .models import Business, Neighbourhood, Profile, Post
+
+# Set up method 
+# Set up method 
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile=Profile(name='juliet')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+#Testing Save Method
+
+    
+
+    
+
+# Set up method        
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post=Post(post='hey')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post)) 
+
+
+    def test_save_post(self):
+        
+        self.post.save()
+        self.assertEqual(len(post.save),1)       
+
+
+
+#  Set up method   
+
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.neighbourhood=Neighbourhood(name='juliet')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood, Neighbourhood))  
+
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business=Business(picture='image')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))           
+
+
+
+
+
+        
+
+
+
+
+
+        
+
+
+
